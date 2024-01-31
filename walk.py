33b5141bf398c7dc372c8f9691e5cdbd21a1b1e2
Matt Lyapin <matthecrazyguy@gmail.com>
2024-01-31 13:44:44 -0500

    added lil walk program

---
+++ walk.py
@@ -0,0 +1,18 @@
+HF, FH, UE, ES, SP, PU, UB, BJ, JB, BE, EB, BU, PC, CS, SU = ["Home to Route 5", 6], ["Route 5 to Home", 7], ["Shuttle-UM 111 to ESJ", 2], ["ESJ to STEM Library", 5], ["STEM Library to Physics Building", 2], ["Physics Building to Shuttle-UM 111", 3], ["Shuttle-UM 111 to Hornbake", 2], ["Hornbake to HJ Patterson", 3], ["HJ Patterson to Hornbake", 3], ["Hornbake to ESJ", 2], ["ESJ to Hornbake", 2], ["Hornbake to Shuttle-UM 111", 2], ["Physics Building to CS Instructional Center", 6], ["CS Instructional Center to STEM Library", 5], ["STEM Library to Shuttle-UM 111", 4]
+
+inp = input("Which day would you like (M/T/F)? ")
+print()
+
+if inp == 'M':
+	wl = [HF, UE, ES, SP, PU, FH]
+elif inp == 'T':
+	wl = [HF, UB, BJ, JB, BE, EB, BU, FH]
+else:
+	wl = [HF, UE, ES, SP, PC, CS, SU, FH]
+
+sum = 0
+for walk in wl:
+	print(f"{walk[0]}: {walk[1]}")
+	sum += walk[1]
+print()
+print(f"Total: {sum}")
